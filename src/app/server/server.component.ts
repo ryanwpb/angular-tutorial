5b0197a8fc6fa80f7f3137507483df3cf48d1c20
Ryan Graham <ryangrahamwpb@gmail.com>
2020-06-16 11:34:29 -0400

    Work with data binding using string interpolaton.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,4 +3,7 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+}
